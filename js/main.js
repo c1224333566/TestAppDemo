ef700a4d12a54be2c9289df67c87e4b64ecc44af
cclove <1224333566@qq.com>
2016-12-01 19:40:53 +0800

    轮播图3

--- js/main.js
+++ js/main.js
@@ -6,17 +6,32 @@ $(function(){
     var $cd = $('#cd');
     var $left = $('#left');
     var $right = $('#right');
+    var $img = $('img');
 
-    $left.css({'display':'none'});
-    $right.css({'display':'none'});
-    $cd.on('mouseenter',function(){
-        $left.css({'display':'block'});
-        $right.css({'display':'block'});
-    })
-    $cd.on('mouseout',function(){
-        $left.css({'display':'none'});
-        $right.css({'display':'none'});
-    })
+    // $cd.css({'background-image':'url("./img/b1.jpg")'});
+
+    function Initsetimg(){
+        var k ;
+        for(k = 1;k<=5;k++){
+            $cd.css({'background-image':'url("./img/b' + k + '.jpg")'});
+            setTimeout(function(){
+                
+            },3000);
+        }
+
+    }
+    Initsetimg();
+
+    // $left.css({'display':'none'});
+    // $right.css({'display':'none'});
+    // $cd.on('mouseenter',function(){
+    //     $left.css({'display':'block'});
+    //     $right.css({'display':'block'});
+    // })
+    // $cd.on('mouseout',function(){
+    //     $left.css({'display':'none'});
+    //     $right.css({'display':'none'});
+    // })
 
 
 })
